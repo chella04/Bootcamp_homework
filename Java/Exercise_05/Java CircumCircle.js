eaa4d9318ded223258e8a28c07e6976e56bbcc2b
chella04 <chella.meena@gmail.com>
2023-04-28 16:25:36 -0400

    Updates

--- Java/Exercise_05/Java CircumCircle.js
+++ Java/Exercise_05/Java CircumCircle.js
@@ -2,13 +2,13 @@ function circumference(radius)
 {
     var circumference = Math.PI * 2*radius;
     console.log("The circumference is " + circumference);
-}git
+}
 
-circumference(5);
+    circumference(5);
 function calcArea(radius)
 {
     var area= Math.PI * radius*radius;
     console.log("The area is"+ area);
 }
     
-calcArea(31.41);
+    calcArea(31.41);
